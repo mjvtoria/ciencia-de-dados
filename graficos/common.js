6596fd4490bc60a1e63f1c55b5e3eca861c3db8d
Marjory <105371524+mjvtoria@users.noreply.github.com>
2024-10-25 08:11:10 -0300

    quase terminando

--- graficos/common.js
+++ graficos/common.js
@@ -2,15 +2,39 @@ const getCSS = (variavel) => {
     return getComputedStyle(document.body).getPropertyValue(variavel)
   };
   
-  const layout = {
-    plot_bgcolor: getCSS("--bg-color"),
-    paper_bgcolor: getCSS("--bg-color")
-    title: {
-        text: "Redes sociais com mais usuários no mundo"
+  // código omitido
+
+const layout = {
+  plot_bgcolor: getCSS("--bg-color"),
+  paper_bgcolor: getCSS("--bg-color"),
+  title: {
+    text: "Redes sociais com mais usuários no mundo",
+     x: 0,
+     font: {
+         color: getCSS("--primary-color"),
+         family: getCSS("--font"),
+         size: 30
+     }
+  },
+    
+  xaxis: {
+      title: {
+          text: "nome das redes sociais",
+          font: {
+              color: getCSS("--secondary-color")
+          }
       }
-
-
+  },
+  yaxis: {
+      title: {
+          text: "bilhões de usuários ativos",
+          font: {
+              color: getCSS("--secondary-color")
+          }
+      }
+  }
 }
+
 
 
   export {getCSS}
