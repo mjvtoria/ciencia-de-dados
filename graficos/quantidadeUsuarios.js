866a475aeaec80c800aaf86af6020a27dee22829
Marjory <105371524+mjvtoria@users.noreply.github.com>
2024-10-25 07:46:25 -0300

    e

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -5,14 +5,16 @@ async function quantidadeUsuarios() {
     const nomeDasRedes = Object.keys(dados)
     const quantidadeUsuarios = Object.values(dados)
   
-  const data = [
-    {
-      x: nomeDasRedes,
-      y: quantidadeUsuarios,
-      type: "bar"
-    }
-  ]
-  
+    const data = [
+        {
+          x: 'nomeDasRedes',
+          y: quantidadeDeUsuarios,
+          type: 'bar',
+          marker: {
+            color: getComputedStyle(document.body).getPropertyValue('--primary-color')
+          }
+        }
+      ]
   const grafico = document.createElement("div")
   grafico.className = "grafico"
   document.getElementById("graficos-container").appendChild(grafico)
