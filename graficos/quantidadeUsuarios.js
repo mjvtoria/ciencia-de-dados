7830005a18c990968d4a481e16977df38b285118
Marjory <105371524+mjvtoria@users.noreply.github.com>
2024-10-25 07:37:33 -0300

    gráfico

---
+++ graficos/quantidadeUsuarios.js
@@ -0,0 +1,11 @@
+async function quantidadeUsuarios() {
+    const url = "https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json"
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeUsuarios = Object.values(dados)
+  
+  }
+  
+  quantidadeUsuarios()
+  
